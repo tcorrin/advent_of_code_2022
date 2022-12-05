f92aca081062d23beb603017abb4ae3d53efd4c0
Tim Corrin <tcorrin@vqcomms.com>
2022-12-05 22:29:00 +0000

    Update day05.go

--- day05/day05.go
+++ day05/day05.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type craneOperation struct {
-	origin      int
-	destination int
+	origin      string
+	destination string
 	quantity    int
 }
 
@@ -40,18 +42,76 @@ func parseCraneOperations(input []string) []craneOperation {
 	result := make([]craneOperation, 0)
 	for _, raw_co := range input {
 		raw_co_split := strings.Split(raw_co, " ")
-		append(result, craneOperation{}
+		q, _ := strconv.Atoi(raw_co_split[1])
+		result = append(result, craneOperation{origin: raw_co_split[3], destination: raw_co_split[5], quantity: q})
+	}
+	return result
+}
+
+func parseCrateStacks(input []string) map[string]string {
+	result := make(map[string]string)
+	stack_labels := input[len(input)-1]
+	stacks := input[:len(input)-1]
+	for label_index, label := range stack_labels {
+		if label != ' ' {
+			result[string(label)] = parseStack(label_index, stacks)
+		}
+	}
+	return result
+}
+
+func parseStack(index int, stack []string) string {
+	result := ""
+	for _, level := range stack {
+		if level[index] != ' ' {
+			result = result + string(level[index])
+		}
+	}
+	return reverseString(result)
+}
+
+func reverseString(input string) (result string) {
+	for _, v := range input {
+		result = string(v) + result
+	}
+	return
+}
+
+func runOperation(operation craneOperation, stacks map[string]string, part_two bool) map[string]string {
+	offset := len(stacks[operation.origin]) - operation.quantity
+	pickup := stacks[operation.origin][offset:]
+	if !part_two {
+		pickup = reverseString(pickup)
+	}
+	stacks[operation.destination] = stacks[operation.destination] + pickup
+	stacks[operation.origin] = stacks[operation.origin][:offset]
+	return stacks
+}
+
+func calcTopRow(stacks map[string]string) string {
+	result := ""
+	for i := 1; i <= len(stacks); i++ {
+		result += string(stacks[strconv.Itoa(i)][len(stacks[strconv.Itoa(i)])-1])
+
 	}
 	return result
 }
 
 func processFile(filename string) {
-	raw_crate_stacks, raw_crane_operations  := loadFile(filename)
+	raw_crate_stacks, raw_crane_operations := loadFile(filename)
 	crate_stacks := parseCrateStacks(raw_crate_stacks)
+	crate_stacks_pt2 := parseCrateStacks(raw_crate_stacks)
 	crane_operations := parseCraneOperations(raw_crane_operations)
 
+	for _, o := range crane_operations {
+		runOperation(o, crate_stacks, false)
+	}
 
-
+	for _, o := range crane_operations {
+		runOperation(o, crate_stacks_pt2, true)
+	}
+	fmt.Println("Top Row: ", calcTopRow(crate_stacks))
+	fmt.Println("Top Row Pt2: ", calcTopRow(crate_stacks_pt2))
 }
 
 func main() {
