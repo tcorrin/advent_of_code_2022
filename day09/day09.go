4b41fb9c8b7385c9a6aa45feb7424bc18f244cf3
Tim Corrin <tcorrin@vqcomms.com>
2022-12-10 16:26:39 +0000

    more day09 progress

--- day09/day09.go
+++ day09/day09.go
@@ -13,6 +13,16 @@ type command struct {
 	amount    int
 }
 
+type knot struct {
+	parent  *position
+	current *position
+}
+
+type position struct {
+	x int
+	y int
+}
+
 func loadFile(filename string) []command {
 	file, err := os.Open(filename)
 	result := make([]command, 0)
@@ -31,13 +41,115 @@ func loadFile(filename string) []command {
 	return result
 }
 
-func processFile(filename string) {
-	
-	 := loadFile(filename)
+func addCurrentTailPosition(tail *position, tail_positions *[]position) {
+	match := false
+	for _, p := range *tail_positions {
+		if p.x == tail.x && p.y == tail.y {
+			match = true
+			break
+		}
+	}
+	if !match {
+		*tail_positions = append(*tail_positions, *tail)
+	}
+}
+
+func processCommand(rope *[]*knot, tail_positions *[]position, c command) {
+	for i := 0; i < c.amount; i++ {
+		moveHead((*rope)[0].current, c.direction)
+		for j := 1; j < len(*rope); j++ {
+			moveKnot((*rope)[j], c.direction)
+		}
+		addCurrentTailPosition((*rope)[len(*rope)-1].current, tail_positions)
+		//fmt.Printf("    Head Position x: %d y: %d - Tail Position x: %d y: %d\n", head.x, head.y, tail.x, tail.y)
+	}
+}
+
+func moveHead(head *position, direction string) {
+	if direction == "R" {
+		head.x += 1
+	} else if direction == "L" {
+		head.x -= 1
+	} else if direction == "U" {
+		head.y += 1
+	} else if direction == "D" {
+		head.y -= 1
+	}
+}
+
+func moveKnot(k *knot, direction string) {
+	if direction == "R" {
+		if k.parent.x > k.current.x+1 {
+			k.current.x += 1
+			if k.parent.y > k.current.y {
+				k.current.y += 1
+			} else if k.parent.y < k.current.y {
+				k.current.y -= 1
+			}
+		}
+	} else if direction == "L" {
+		if k.parent.x < k.current.x-1 {
+			k.current.x -= 1
+			if k.parent.y > k.current.y {
+				k.current.y += 1
+			} else if k.parent.y < k.current.y {
+				k.current.y -= 1
+			}
+		}
+	} else if direction == "U" {
+		if k.parent.y > k.current.y+1 {
+			k.current.y += 1
+			if k.parent.x > k.current.x {
+				k.current.x += 1
+			} else if k.parent.x < k.current.x {
+				k.current.x -= 1
+			}
+		}
+	} else if direction == "D" {
+		if k.parent.y < k.current.y-1 {
+			k.current.y -= 1
+			if k.parent.x > k.current.x {
+				k.current.x += 1
+			} else if k.parent.x < k.current.x {
+				k.current.x -= 1
+			}
+		}
+	}
+}
+
+func printRope(rope []*knot) {
+	for i, k := range rope {
+		if k.parent != nil {
+			fmt.Printf("Knot %d Current x: %d y: %d Parent x: %d y: %d\n", i, k.current.x, k.current.y, k.parent.x, k.parent.y)
+		} else {
+			fmt.Printf("Knot %d Current x: %d y: %d\n", i, k.current.x, k.current.y)
+		}
+	}
+}
+
+func processFile(filename string, rope_length int) {
+	tail_positions := make([]position, 0)
+	rope := make([]*knot, 1)
+	rope[0] = &knot{parent: nil, current: &position{x: 0, y: 0}}
+	for i := 1; i < rope_length; i++ {
+		rope = append(rope, &knot{parent: rope[i-1].current, current: &position{x: 0, y: 0}})
+	}
+	tail_positions = append(tail_positions, *rope[0].current)
+	command_list := loadFile(filename)
+	printRope(rope)
+	for _, c := range command_list {
+		processCommand(&rope, &tail_positions, c)
+		printRope(rope)
+	}
+	fmt.Println("Number of unique tail positions: ", len(tail_positions))
 }
 
 func main() {
-	processFile("day09-example.txt")
-	fmt.Println("-------")
-	processFile("day09.txt")
+	//processFile("day09-example.txt", 2)
+	//processFile("day09.txt", 2)
+	//fmt.Println("-------")
+	processFile("day09-example.txt", 10)
+	//processFile("day09-example02.txt", 10)
+	//processFile("day09.txt", 10)
+
 }
